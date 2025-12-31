906420d978abd2f6643eea0212de3079c77e7c88
Abhijeet-Raj264 <abhijeetraj306@gmail.com>
2025-12-31 20:15:48 +0530

    Model Training and Model Evaluation Completed

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from network_security.logging.logger import logging
 from network_security.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from network_security.entity.config_entity import TrainingPipelineConfig
 
-#from network_security.components.model_trainer import ModelTrainer
+from network_security.components.model_trainer import ModelTrainer
 from network_security.entity.config_entity import ModelTrainerConfig
  
 
@@ -33,6 +33,13 @@ if __name__=='__main__':
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("data transformation completed")
+
+        logging.info("Model Training started")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training Artifact Created")
 
     except Exception as e:
            raise NetworkSecurityException(e,sys)
